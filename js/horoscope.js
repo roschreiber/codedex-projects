60b1fe3d852987a6b830c77fddc16ad7a9f8ac81
roschreiber <schreiber.robin@icloud.com>
2024-03-29 18:22:53 +0100

    Horoscope Checkpoint
    Javascript

---
+++ js/horoscope.js
@@ -0,0 +1,30 @@
+let month = "";
+
+if (month == "January") {
+    console.log("🔮: Capricorn ♑ | Responsible, disciplined, self-controlled, and ambitious.");
+} else if (month == "February") {
+    console.log("🔮: Aquarius ♒ | Progressive, original, independent, and humanitarian.");
+} else if (month == "March") {
+    console.log("🔮: Pisces ♓ | Compassionate, artistic, intuitive, and gentle.");
+} else if (month == "April") {
+    console.log("🔮: Aries ♈ | Independent, passionate, adventurous, and courageous.");
+} else if (month == "May") {
+    console.log("🔮: Taurus ♉ | Reliable, patient, practical, and determined.");
+} else if (month == "June") {
+    console.log("🔮: Gemini ♊| Adaptable, curious, witty, and expressive.");
+} else if (month == "July") {
+    console.log("🔮: Cancer ♋ | Nurturing, intuitive, emotional, and protective.");
+} else if (month == "August") {
+    console.log("🔮: Leo ♌ | Confident, generous, loyal, and theatrical.");
+} else if (month == "September") {
+    console.log("🔮: Virgo ♍ | Analytical, practical, detail-oriented, and modest.");
+} else if (month == "October") {
+    console.log("🔮: Libra ♎ | Diplomatic, social, cooperative, and fair-minded.");
+} else if (month == "November") {
+    console.log("🔮: Scorpio ♏ | Passionate, resourceful, determined, and mysterious.");
+} else if (month == "December") {
+    console.log("🔮: Sagittarius ♐ | Optimistic, adventurous, philosophical, and humorous.");
+} else {
+    console.log("🔮: That's not a valid month, try again. 🤔");
+}
+
